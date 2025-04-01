68dede1a690a1559b578bc0e0dc8d2f161906f6c
abhinav6859 <abhinavvats06665@gmail.com>
2025-04-02 00:28:09 +0530

    Update

--- 8_props-contact/src/components/App.jsx
+++ 8_props-contact/src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Detail from "./detail";
 import contacts from "../contacts";
+import Avatar from "./avatar";
+
+function createcard(contact) {
+  return (
+    <Detail
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
 
-      <Detail
+      {contacts.map(createcard)}
+      {/* <Detail
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -23,7 +39,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
